f43f4e742948db61d3af5b8d9e5effd626a8427b
Daniel Mieszkowski (Sii) <dmieszkowski@sii.pl>
2023-09-14 10:36:42 +0200

    remove d.ts

--- src/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare class Superppmds {
-    constructor();
-    private improveRoleManager;
-}
-declare const superppmda: Superppmds;
-declare function waitForElm(selector: any): Promise<unknown>;
