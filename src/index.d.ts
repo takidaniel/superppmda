c6eabba8827968b81d6692cd09794733fb49518e
Daniel Mieszkowski (Sii) <dmieszkowski@sii.pl>
2023-09-14 10:34:30 +0200

    addd all dynamics.com

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+declare class Superppmds {
+    constructor();
+    private improveRoleManager;
+}
+declare const superppmda: Superppmds;
+declare function waitForElm(selector: any): Promise<unknown>;
